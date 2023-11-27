7858d35f54de71de11d015f651974d47961b1303
alxmrf <alxmrf04@gmail.com>
2023-11-27 01:39:59 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-export default function home() {
+"use client"
+
+
+import { useState } from "react";
+
+
+export default function Home() {
+
+
+const [VidaPlayer1, setVidaPlayer1] = useState(100)
+const [VidaPlayer2, setVidaPlayer2] = useState(100)
+const [VidaPlayer3, setVidaPlayer3] = useState(100)
+const [placeholder1, setPlaceholder1] = useState(100)
+const [placeholder2, setPlaceholder2] = useState(100)
+const [placeholder3, setPlaceholder3] = useState(100) 
+
+const handleLifeChange = () => {
+  setVidaPlayer1(() => {
+    return placeholder1
+  })	}
+
   return (
-    <div>
-      <h1>Home</h1>
+    <>
+    <div className="flex justify-center gap-x-8 ">
+        <h1>Player name</h1>
+        <h1>health</h1> 
+        <h1> -------------------</h1>
+        <h1> -------------------</h1>
     </div>
-  );
-}
+    <div className="flex justify-center gap-x-8">
+        <h1>Player 1</h1>
+        <h1>{VidaPlayer1}</h1>
+        <input type="text" value={placeholder1} onChange={(e)=>setPlaceholder1(event.target.value)} />
+        <button className="rounded" onClick={handleLifeChange}>submit</button>
+    </div>
+  </>)}
